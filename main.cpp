3c9072b77198910f2976f408544f6b1453534d77
Swapnil Singh <swapnil.singh@g.austincc.edu>
2023-10-03 15:19:26 -0500

    comment out testing code in main

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main() {
     cout << "creating the linked list...";
     LinkedList list;
     cout << "done" << endl << endl;
-
+    /*
     cout << "checking list...\n";
     cout << "\tThere are " << list.getCount() << " nodes." << endl;
     list.printList();
@@ -112,7 +112,7 @@ int main() {
     cout << "\tThere are " << list.getCount() << " nodes." << endl;
     list.printList();
     cout << endl;
-
+*/
 
     return 0;
 }
