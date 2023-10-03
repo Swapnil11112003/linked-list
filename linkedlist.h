6e640fee1c280c2b383e1c36435ead36c5f8eff4
Swapnil Singh <swapnil.singh@g.austincc.edu>
2023-10-03 15:13:06 -0500

    create skeleton class for linked list

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,20 @@
 /*
- * your comment header here
+ * Header file
  */
+
+/*
+ * File name: linkedlist.h
+ * Author: Swapnil Singh
+ */
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "data.h"
+
+class LinkedList {
+    private:
+        Node *head;
+};
+
+#endif //LINKEDLIST_H
